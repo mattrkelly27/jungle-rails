9d223db6c381d8a2943dc80f78c33ec7f0e72577
Matt Kelly <matt.r.kelly27@gmail.com>
2019-02-21 19:59:45 +0000

    test commit

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,15 +18,13 @@ class Admin::CategoriesController < ApplicationController
       end
 
       private
-      
+
 
       def category_params
         params.require(:category).permit(
           :name
         )
       end
- 
-    
-    
+
     
 end
