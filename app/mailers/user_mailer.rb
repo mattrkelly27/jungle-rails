aed3fbcf0b719d654c97697fe5389b66d9edfa6a
Matt Kelly <matt.r.kelly27@gmail.com>
2019-02-22 20:01:37 +0000

    test commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,13 @@
 class UserMailer < ApplicationMailer
     default from: 'no-reply@jungle.com'
    
+
+
+    
     def welcome_email(order)
       @order = order
       mail(to: @order.email, subject: 'Welcome to My Awesome Site')
     end
   end
+
+
